d1997ffc219f4ab9496f513da5b7a0b16bc57a94
Cathy Hsu <chsucathy@gmail.com>
2020-09-04 19:13:04 -0700

    Uncomment statements in LandingTest

--- app/src/test/java/com/example/loginassignment/LandingTest.java
+++ app/src/test/java/com/example/loginassignment/LandingTest.java
@@ -9,12 +9,12 @@ import static org.junit.Assert.assertTrue;
 // Citation: https://stackoverflow.com/questions/9445661/how-to-get-the-context-from-anywhere
 public class LandingTest {
 
-    // error: putExtra not mocked
-//    @Test
-//    public void shouldReturnIntent() {
-//        Landing landing = new Landing();
+    // Error: putExtra method not mocked
+    @Test
+    public void shouldReturnIntent() {
+        Landing landing = new Landing();
 //        Intent intent = landing.getIntent(MainActivity.getContext(), "din_djarin");
 //        assertTrue(intent.getClass() == Intent.class);
-//    }
+    }
 }
 
